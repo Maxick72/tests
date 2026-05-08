6dfd0f77946a1c384450c017911063c5c90d97f6
Mikhail Petrov <mpetrov71@mail.ru>
2026-05-08 13:15:04 +0500

    test_main.py modify

---
+++ tests/test_main.py
@@ -0,0 +1,29 @@
+import pytest
+from src.main import check_age
+from src.main import check_auth
+from src.main import check_triangle
+
+
+@pytest.mark.parametrize(
+    'age,expected_authorization',
+     ((10,'Доступ запрещён'),
+      (18,'Доступ разрешён'),
+      (17,'Доступ запрещён'),
+      (45,'Доступ разрешён'))
+)
+
+
+def test_check_age(age, expected_authorization):
+    assert check_age(age) == expected_authorization, f'Получен неверный ответ для возраста: {age}'
+
+
+@pytest.mark.parametrize(
+    'login, password, expected_auth',
+    (('admin', 'password', 'Добро пожаловать'),
+     ('user', 'password', 'Доступ ограничен'),
+     ('admin', 'P@ssw0rd', 'Доступ ограничен'))
+)
+
+
+def test_check_auth(login,password,expected_auth):
+    assert check_auth(login, password) == expected_auth, f' Получен неверный ответ для аутентификации: {login}'
